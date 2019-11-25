8fb4c769a3e3bcf4055676e9b43c20daf982cef8
Trần Ngọc Qúy (VINID-KCN-MB) <quytn3@vingroup.net>
2019-11-25 09:22:49 +0700

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-board';
+  nameList: string;
+  limitJob: number | boolean;
+  lists: Array <any> = [];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+  addList () {
+    this.nameList && this.lists.push({
+      name: this.nameList,
+      limit: this.limitJob,
+      jobs: []
+    });
+  }
+
+  deleteList (idx) {
+    this.lists.splice(idx, 1);
+  }
+
+  addItem (listIdx) {
+    const list = this.lists[listIdx];
+    list.nameJobs &&
+    (list.jobs.length < list.limit || list.limit === undefined) &&
+    list.jobs.push(list.nameJobs);
+  }
+
+  deleteItem (listIdx, jobIdx) {
+    this.lists[listIdx].jobs.splice(jobIdx, 1);
+  }
+
+  cdkDropListConnectedTo (idx) {
+    return this.lists.reduce(
+      (s, e, i) => {
+        if (i != idx) {
+          s.push("list_" + i + "");
+        }
+        return s;
+      },
+      []
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule, MatGridListModule, MatDividerModule, MatListModule, MatButtonModule, MatInputModule, DragDropModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
